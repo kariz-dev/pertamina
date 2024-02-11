df7f0def0476f2acb982f859c0637d419913e473
Rizal <emrizal21@gmail.com>
2024-02-11 08:06:57 +0700

    berita partner galery masih nanggung

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,30 @@ $(document).ready(function(){
     slidesToShow: 2,
     dots: true
   });
+  $('.partner-carousel').slick({
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }]
+  })
 });
